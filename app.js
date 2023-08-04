f6c14491b237bd57ccae8ffb66781130c6b56d0c
Nehrasaisurya <nehrasais12@gmail.com>
2023-08-04 19:56:00 +0530

    Changes

---
+++ app.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from react");
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"Iam H1 tag"),React.createElement("h2",{},"Iam H2 tag")]));
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
