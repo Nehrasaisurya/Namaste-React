bb2a850e4164fd7a9970309d82e5dcca350411c6
Nehrasaisurya <nehrasais12@gmail.com>
2023-08-05 17:43:33 +0530

    commit

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
+ 
+const parent=React.createElement("h1",{},"HI Iam Hello");
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from react");
-
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"Iam H1 tag"),React.createElement("h2",{},"Iam H2 tag")]));
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent)
+root.render(parent);
